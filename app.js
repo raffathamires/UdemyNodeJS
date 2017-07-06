4238bdc422484e46393e4185f62c8e30fc376e17
raffathamires <raffaela.monteiro@gmail.com>
2017-07-05 23:34:22 -0300

    EJS

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 var express = require('express');
 var app = express();
 
+//inicializando o EJS
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render("secao/home");
+});
+
+app.get('/tecnologia', function(req, res){
+    res.render("secao/tecnologia");
+});
+
 app.listen(3000, function(){
     console.log("Servidor rodando com Express");
 });
