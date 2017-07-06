b3e0a754f75253ffe7859c79a5dea4b881c0cdb9
raffathamires <raffaela.monteiro@gmail.com>
2017-07-05 23:11:17 -0300

    Express

--- noticias.js
+++ noticias.js
@@ -4,10 +4,21 @@ var http = require('http');
 
 //funcoes como argumento
 var server = http.createServer( function(req, res){
-    //resposta normalmente em html
-    res.end("<html><body>Portal de Noticias</body></html>");
 
-
+    var categoria = req.url;
+    //response = resposta normalmente em html
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Noticias de tecnologia</body></html>");
+    }
+    else if(categoria == '/moda'){
+         res.end("<html><body>Noticias de moda</body></html>");
+    }
+    else if(categoria == '/beleza'){
+         res.end("<html><body>Noticias de beleza</body></html>");
+    }
+    else{
+        res.end("<html><body>Portal de Noticias</body></html>");
+    }
 });
 
 //porta que o server vai "escutar"
