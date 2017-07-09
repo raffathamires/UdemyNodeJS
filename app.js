580c1a287771ac983c0d36f17e89a0827597a064
raffathamires <raffaela.monteiro@gmail.com>
2017-07-09 16:09:15 -0300

    refactoring

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.render("secao/home");
+    res.render("home/index");
 });
 
-app.get('/tecnologia', function(req, res){
-    res.render("secao/tecnologia");
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+    res.render("noticias/noticias");
 });
 
 app.listen(3000, function(){
