087226ba847880f959713d01684550ebcd0a7567
raffathamires <raffaela.monteiro@gmail.com>
2017-07-15 21:28:40 -0300

    Formulario

--- config/server.js
+++ config/server.js
@@ -4,12 +4,17 @@ var express = require('express');
 //utilizando o consign
 var consign = require('consign');
 
+//utilizando body-parser
+var bodyParser = require('body-parser')
+
 //chamada do express
 var app = express();
 
 //inicializando o EJS
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended : true}));
 
 consign()
     .include('./app/routes')
